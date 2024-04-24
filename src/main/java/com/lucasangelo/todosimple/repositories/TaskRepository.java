1048951e2401d1b3d8dd83e811c5cad385def358
Fernanda Helen <fhplira@gmail.com>
2024-04-24 16:04:02 -0300

    Task and User repositories created

---
+++ src/main/java/com/lucasangelo/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.lucasangelo.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasangelo.todosimple.models.Task;
+import java.util.List;
+
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
+ 
+    List<Task> findByUser_Id(Long id);
+
+}
